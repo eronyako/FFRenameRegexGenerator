6595027e2813642237b4f14abc22f0b25c56ce0d
eronyako <1138345264@qq.com>
2022-08-16 15:58:12 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # FFRenameRegexGenerator
+
 本项目为使用 python3 编写的 FFRenamePro 的正则表达式更名命令批量生成器。
+
+## 使用
+本项目可以使用直接使用 start.py 文件（需要安装 Python3 并添加到环境变量），或使用 Releases 的 exe 文件。
+
+所需文件如下：
+
+- 程序主体：start.py / FFRenameRegexGenerator.exe
+- 正则表达式列表：regx.txt
+
+### 正则表达式列表要求
+
+文件需要使用 UTF-8 Without BOM 编码。
+
+文件内容的格式如下：
+
+```
+# 这是注释
+(.*)(\(.*[漢汉]化.?\))(.*)
+$1$3
+```
+
+- 无内容的行不处理，跳过
+- `#` 开头的行为注释行，不处理，跳过
+- `(` 开头的行视为匹配正则表达式
+- `$` 开头的行视为替换后的模板
+- 除上述外，任何字符开头的行被视为错误，请严格按照上述开头调整
+
+  >`$1` 匹配正则表达式第一个括号内的内容，`$2` 匹配第二个括号，依此类推。
+  > 
+  > 正则表达式的下方必须是模板行，可以有注释和空行分割，但不能再次出现正则表达式。
+  > 
+  > 上部的代码表示把文件名中 `汉化` 或 `漢化` 挨着右括号或间隔一个字符的小括号里面的内容删除。
+
+### 使用源码
+
+确认 regx.txt 与 FFRenameRegexGenerator.exe 在同一个目录
+
+在当前目录运行：
+
+```shell
+python start.py
+```
+
+### 使用封装版
+
+确认 regx.txt 与 FFRenameRegexGenerator.exe 在同一个目录
+
+直接运行 FFRenameRegexGenerator.exe
+
+## 输出
+
+若正常运行后将会在当前文件夹生成 regex.frc 文件，可将此文件导入 FFRenamePro 中运行批处理命名。
---
+++ start.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Subj: FFRenameRegxGenerator
+# @File: start.py
+# @Date: 2022/8/16 11:23
+
+# 读取正则表达式列表
+
+print('读取文件...')
+try:
+    f = open('regex.txt', 'r', encoding='UTF-8')
+except FileNotFoundError:
+    print('regex.txt 文件未找到')
+    raise FileNotFoundError('regex.txt 文件未找到')
+except UnicodeDecodeError:
+    print('编码不正确，请使用 UTF-8 (without BOM) 编码的文件')
+    raise UnicodeError('编码不正确，请使用 UTF-8 (without BOM) 编码的文件')
+else:
+    regex_list = f.readlines()
+    f.close()
+
+# 去除换行
+i = 0
+for v in regex_list:
+    regex_list[i] = v.strip()
+    i += 1
+
+regex = []
+i = 0
+for v in regex_list:
+    if v == '':
+        continue
+    elif v[0] == '#':
+        continue
+    elif v[0] == '(':
+        regex.append([])
+        regex[i].append(v)
+    elif v[0] == '$':
+        regex[i].append(v)
+        i += 1
+    else:
+        raise ValueError('不是有效的正则式列表')
+
+# 生成 FFRename Pro 的正则表达式 frc 文件
+
+date0 = 'CMD_A120220816'
+date1 = 103514300
+
+file0 = ';===========================================================\n' \
+        ';批处理命令文件\n;菲菲更名宝贝 之 得意非凡 (FFRename Professional)\n;菲菲的家(ffhome.com)版权所有\n;\n' \
+        ';说明：本文件为系统生成，除非明白各参数之意义，否则请不要随\n;      意改动，以免影响原有的更名结果！\n' \
+        ';===========================================================\n'
+
+file1 = '[CommandList]\nCommandListCount=' + str(len(regex)) + '\n'
+cmd_list = []
+i = 1
+while i <= len(regex):
+    cmd_list.append(date0 + str(date1))
+    file1 = file1 + str(i) + '=' + date0 + str(date1) + '\n'
+    i += 1
+    date1 += 1000
+
+i = 0
+file2 = ''
+for v in cmd_list:
+    file2 = file2 + '\n[' + v + ']\nCMDComment=正则表达式更名\nCMDid=A101\nA1_Regex=1\nA1_RegexText=|'
+    file2 = file2 + regex[i][0] + '|\nA1_RegexReplaceText=|' + regex[i][1]
+    file2 = file2 + '|\nA1_RegexIncludeExt=0\nA1_RegexReplaceAll=0\nA1_RegexReplaceSome=0\n' \
+                    'A1_RegexReplaceSomeText=1\nA1_RegexReplaceSomeGetOnly=1\n' \
+                    'A1_RegexReplaceSomeGetOnlyText=1\n'
+    i += 1
+
+file = file0 + file1 + file2
+with open('regex.frc', 'w', encoding='UTF-8-SIG') as f:
+    f.write(file)
+
+print('已生成文件 regex.frc')
